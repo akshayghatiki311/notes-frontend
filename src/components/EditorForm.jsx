8bbe78ec92ff022bd68879852bfb8e9414a88e61
Akshay Ghatiki <akshayghatiki@Akshays-MacBook-Pro.local>
2025-01-09 19:34:58 +0530

    changed debounce value

--- src/components/EditorForm.jsx
+++ src/components/EditorForm.jsx
@@ -37,7 +37,7 @@ export default function EditorForm({ note, onSave }) {
       if (WebSocketService.isConnected()) {
         WebSocketService.send(data);
       }
-    }, 600),
+    }, 300),
     []
   );
 
